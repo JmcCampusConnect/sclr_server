27179d84b32a67b90fc146f2610a412d1cba5b82
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-10-19 12:34:27 +0530

    Donar Schema

--- models/Donar.js
+++ models/Donar.js
@@ -1,1 +1,22 @@
-console.log('Donar')
+const mongoose = require('mongoose');
+
+const donorSchema = new mongoose.Schema({
+    academicYear: String,
+    donorId: String,
+    donorName: String,
+    mobileNo: Number,
+    emailId: String,
+    panNo: String,
+    address: String,
+    district: String,
+    state: String,
+    pinCode: Number,
+    donorType: String,
+    donorDate: String,
+    generalAmt: Number,
+    generalBal: Number,
+    zakkathAmt: Number,
+    zakkathBal: Number,
+}, { timestamps: true });
+
+module.exports = mongoose.model('donor', donorSchema)
