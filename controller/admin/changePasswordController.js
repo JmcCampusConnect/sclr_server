1dafa511b64128581d6caf2c23b9058bbd1163cb
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-11-17 23:16:19 +0530

    Staff Password

--- controller/admin/changePasswordController.js
+++ controller/admin/changePasswordController.js
@@ -8,29 +8,25 @@ const changePassword = async (req, res) => {
 
     try {
 
-        const {password} = req.body;
+        const { password } = req.body;
 
+        const staff = await StaffModel.findOne({ staffId: "ADMIN" });
 
-        // Find staff member
-        const staff = await StaffModel.findOne({staffId: "ADMIN"});
         if (!staff) {
-            return res.status(404).json({message: 'Staff member not found'});
+            return res.status(404).json({ message: 'Staff member not found' });
         }
 
-
-        // Update password
         staff.password = password;
         await staff.save();
-
-        res.status(200).json({message: 'Password changed successfully'});
+        res.status(200).json({ message: 'Password changed successfully' });
 
     } catch (error) {
         console.error('Error changing password: ', error);
-        res.status(500).json({message: 'Error changing password'});
+        res.status(500).json({ message: 'Error changing password' });
     }
 
 }
 
 // ----------------------------------------------------------------------------------------------------------------
 
-module.exports = {changePassword};
+module.exports = { changePassword };
--- routes/admin/changePasswordRoutes.js
+++ routes/admin/changePasswordRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {changePassword} = require('../../controller/admin/changePasswordController');
+const { changePassword } = require('../../controller/admin/changePasswordController');
 
 // -----------------------------------------------------------------------------------------------------------------
 
