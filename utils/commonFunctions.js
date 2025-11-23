7928f7f2cc77f481cef6b92afcc5bd4771978407
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-11-23 18:13:08 +0530

    Common Functions

--- utils/commonFunctions.js
+++ utils/commonFunctions.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const AcademicModel = require("../models/Academic");
 const ApplicationModel = require("../models/Application");
 const DepartmentModel = require("../models/Department");
+const DonorModel = require("../models/Donor");
 
 // -----------------------------------------------------------------------------------------------------------------
 
@@ -20,11 +21,11 @@ const currentAcademicYear = async () => {
 router.get("/fetchDropdownData", async (req, res) => {
 
     try {
-
         const batches = await ApplicationModel.distinct("yearOfAdmission");
         const departments = await DepartmentModel.find().select("department departmentName");
         const categories = await ApplicationModel.distinct("category");
-        res.json({ batches, departments, categories });
+        const donors = await DonorModel.find().select("donorId donorName");
+        res.json({ batches, departments, categories, donors });
     } catch (error) {
         console.error("Dropdown Error:", error);
         return res.status(500).json({ message: "Server error while fetching dropdown data" });
