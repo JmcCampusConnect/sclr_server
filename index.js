42e8b8797236666dd9707e16462c08abf5277b27
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-12-18 22:36:10 +0530

    Correction

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true
+    credentials: true 
 }))
 
 connectDB();
